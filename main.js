94626260bcff7ff71aa7a2efba0b4477a4e9a7bf
alejandro Muñoz <alex.brus36@gmail.com>
2023-03-20 01:49:56 +0000

    clase 23 segundo aside y shopingCard se fusionan estilos, JS y html

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
 const menuHamIcon = document.querySelector(".menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
@@ -11,27 +11,33 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
+
   if (!isAsideClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
+
   desktopMenu.classList.toggle("inactive");
 }
+
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
+  const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
+
   if (!isAsideClosed) {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
   }
+
   mobileMenu.classList.toggle("inactive");
 }
+
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
 
-  //aside.classList.toggle("inactive");
   if (!isMobileMenuClosed) {
     mobileMenu.classList.add("inactive");
   }
-  aside.classList.toggle("inactive");
+
+  shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
@@ -53,11 +59,13 @@ productList.push({
   image:
     "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
+
 function renderProducts(arr) {
   for (product of arr) {
     const productCard = document.createElement("div");
     productCard.classList.add("product-card");
 
+    // product= {name, price, image} -> product.image
     const productImg = document.createElement("img");
     productImg.setAttribute("src", product.image);
 
@@ -68,7 +76,6 @@ function renderProducts(arr) {
 
     const productPrice = document.createElement("p");
     productPrice.innerText = "$" + product.price;
-
     const productName = document.createElement("p");
     productName.innerText = product.name;
 
@@ -76,16 +83,19 @@ function renderProducts(arr) {
     productInfoDiv.appendChild(productName);
 
     const productInfoFigure = document.createElement("figure");
-
     const productImgCart = document.createElement("img");
     productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
 
     productInfoFigure.appendChild(productImgCart);
+
     productInfo.appendChild(productInfoDiv);
     productInfo.appendChild(productInfoFigure);
+
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
+
     cardsContainer.appendChild(productCard);
   }
 }
+
 renderProducts(productList);
